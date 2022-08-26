dc1660a58552c620df817d9dfc02f550bfa27b55
Denis Ladeira <105209483+denisfloyd@users.noreply.github.com>
2022-08-26 18:42:54 -0300

    test: add test cases to queryParam method

--- 1-module/src/lib/queryString.spec.js
+++ 1-module/src/lib/queryString.spec.js
@@ -1,12 +1,33 @@
 const { queryString } = require("./queryString");
 
 describe("Object to query string", () => {
-  it("should create a valid query string when a object is valid", () => {
+  it('should create a valid query string when a object is valid', () => {
     const obj = {
-      name: "Denis",
-      profession: "developer",
+      name: 'Denis',
+      profession: 'developer',
     };
 
     expect(queryString(obj)).toBe('name=Denis&profession=developer');
+  });
+
+  it('should create a valid query string even when an array is passed as value', () => {
+    const obj = {
+      name: 'Denis',
+      abilities: ['JS', 'TDD'],
+    };
+
+    expect(queryString(obj)).toBe('name=Denis&abilities=JS,TDD');
+  });
+
+  it('should throw an error when an object is passed as value', () => {
+    const obj = {
+      name: 'Denis',
+      abilities: {
+        first: 'JS',
+        second: 'TDD',
+      },
+    };
+
+    expect(() => queryString(obj)).toThrowError();
   });
 });
