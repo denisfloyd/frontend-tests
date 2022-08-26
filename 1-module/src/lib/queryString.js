89e46e4a05ab46aa43c84a66050fdef4d26eca2d
Denis Ladeira <105209483+denisfloyd@users.noreply.github.com>
2022-08-26 18:36:58 -0300

    refactor: refactor method query string

--- 1-module/src/lib/queryString.js
+++ 1-module/src/lib/queryString.js
@@ -1,11 +1,10 @@
-module.exports.queryString = (obj) => {
-  return Object.entries(obj)
+module.exports.queryString = obj =>
+  Object.entries(obj)
     .map(([key, value]) => {
-      if (typeof value === "object" && !Array.isArray(value)) {
-        throw new Error("Please check yor params!");
+      if (typeof value === 'object' && !Array.isArray(value)) {
+        throw new Error('Please check yor params!');
       }
 
       return `${key}=${value}`;
     })
-    .join("&");
-};
+    .join('&');
